7fd0787ea0759057269facae59aafa13a10c3e1e
liviu-o <li.viu@mail.com>
2023-07-12 10:50:09 +0100

    Add hover and leave functionality on stars

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -18,6 +18,8 @@ const textStyle = {
 
 function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
@@ -25,11 +27,13 @@ function StarRating({ maxRating = 5 }) {
           <Star
             key={i}
             onRate={() => setRating(i + 1)}
-            full={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
