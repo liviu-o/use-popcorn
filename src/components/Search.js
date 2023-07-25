822a3c68ec306c00b6ddbe82ed729f1a786004e9
liviu-o <li.viu@mail.com>
2023-07-25 14:23:58 +0100

    Add useRef hook to search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
   return (
     <input
       className="search"
@@ -8,6 +24,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
